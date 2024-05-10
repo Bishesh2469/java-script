6e13e1402e11ddbdb4957f6dd5402a469e1e4f33
bishesh2469 <bishesh246@gmail.com>
2024-05-10 09:29:19 +0545

    introduction to java script

---
+++ index.js
@@ -0,0 +1,57 @@
+console.log(hello)
+console.log("Try programiz.pro");
+
+//variable ...
+
+
+//container/box - means esle kura store garna milo...
+greetings = "namaste";
+name = "tede";
+age = ""
+console.log("greeting");
+console.log("name");
+
+
+//reserved keywords
+//const="hello"
+//for="ram"
+//if="sanchai"
+
+fullname = "shyam bahadur";
+
+eutaofficekoaddress = "balaju";
+
+euta_office_ko_address = "sukedahra";// snake case
+eutaOFFICEkoADDRESS = "ramechaap resturent"//camel case
+console.log(eutaofficekoaddress);
+console.log(euta_office_ko_address);
+console.log(eutaOFFICEkoADDRESS);
+
+//variable ma kk kura rakhna milxa teslai chai data types bhanxa...
+productprice = 100;
+tarkariKoPrice
+
+issingle = false;
+/*
+dataTypes
+1.sring;
+2.number
+integer
+float/double
+3.boolean
+true
+false
+*/
+//*varible banaune practice
+
+//const
+//let
+//var sakbar use naagareko ramro
+
+var laptopKoBrand="asus";
+console.log("laptop ko brand",laptopKoBand)
+
+const PI=3.14;
+console.log("pi o value", PI)
+
+
